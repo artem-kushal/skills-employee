aa9a215cab004e3e57ce5006601fedb2d4ea2398
unknown <stdart9@gmail.com>
2015-11-12 15:04:49 +0300

    add test mongodb connection

--- server/main.js
+++ server/main.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/data');
+
 app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
+    var collection = db.get('usercollection');
+    collection.find({},{limit:20},function(e,docs){
+	    res.json(docs);
+	});
 })
 
 var server = app.listen(8081, function () {
@@ -14,6 +18,6 @@ var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Server listening at http://%s:%s", host, port)
 
 })
