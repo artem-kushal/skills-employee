6993237092706af97826d109a3e00e9db6fd869c
Artsiom Kushal <artsiom.kushal@icx.local>
2016-02-09 10:33:58 +0300

    add gulp build task

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -3,27 +3,41 @@
 var gulp = require('gulp');
 var gulpLoadPlugins = require('gulp-load-plugins')
 var plugins = gulpLoadPlugins();
+var imageminPngquant = require('imagemin-pngquant');
 var express = require('express'),
     app = express(),
     server = require('http').Server(app),
     bodyParser = require('body-parser');
 
 var path = {
+    src: { 
+        html: 'app/*.html', 
+        partials: 'app/partials/*.html',
+        js: 'app/js/*.js',
+        style: 'app/css/style.css',
+        img: 'app/images/**/*.*', 
+        fonts: ['app/bower_components/materialize/font/**/*.*', 'app/font/*.*']
+    },
     watch: {
         html: 'app/**/*.html',
         partials: 'app/partials/**/*.html',
         js: 'app/js/**/*.js',
         style: 'app/css/*.css',
         img: 'app/images/**/*.*',
-        fonts: 'app/font/**/*.*',
-        bower: 'bower.json'
+        fonts: 'app/font/**/*.*'
     },
-    clean: 'build',
-    src_folder: {
-        js: 'app/js',
-        bower: 'app/bower_components'
-    }
+    build: { 
+        html: 'build/',
+        partials: 'build/partials/',
+        js: 'build/js/',
+        style: 'build/css/',
+        img: 'build/images/',
+        fonts: 'build/font/'        
+    },
+    clean: 'build'
 };
+
+// -------------------------- task for watching changes and livereload --------------------------
 
 gulp.task('server', function () {
     app.use(bodyParser.json());
@@ -33,11 +47,11 @@ gulp.task('server', function () {
     app.use(require('connect-livereload')({
         port: 35729
     }));
-    app.use(express.static(__dirname + '/app')); // для того чтобы смог найтись путь к js, css и т. д.
+    app.use(express.static(__dirname + '/build')); // для того чтобы смог найтись путь к js, css и т. д.
     app.use('/*', function (req, res) {
-        res.sendFile(__dirname + '/app/index.html');
+        res.sendFile(__dirname + '/build/index.html');
     });
-    server.listen(3000, function () {
+    server.listen(5000, function () {
         console.log('Server start on port %d', server.address().port);
     });
 });
@@ -62,7 +76,71 @@ gulp.task('watch', function () {
     plugins.watch([path.watch.js], notifyLiveReload);
     plugins.watch([path.watch.partials], notifyLiveReload);
     plugins.watch([path.watch.style], notifyLiveReload);
-    // plugins.watch([path.watch.bower], ['libs']);
 });
 
 gulp.task('start', ['server', 'livereload', 'watch']);
+
+// ---------------------- task for build --------------------------
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) 
+        .pipe(gulp.dest(path.build.html)); 
+});
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) 
+        .pipe(plugins.uglify()) 
+        .pipe(gulp.dest(path.build.js));
+});
+
+gulp.task('libs:build', function () {
+    gulp.src('app/index.html')
+        .pipe(plugins.useref())
+        // .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest(path.build.html));
+});
+
+gulp.task('style:build', function () {
+    gulp.src(path.src.style) 
+        .pipe(plugins.minifyCss({rebase: false, keepSpecialComments: 0}))
+        .pipe(plugins.autoprefixer({browsers: ['last 2 versions'], cascade: false})) 
+        // .pipe(plugins.csso())
+        .pipe(gulp.dest(path.build.style));
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img) //Выберем наши картинки
+        .pipe(plugins.imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [imageminPngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img));
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts));
+});
+
+gulp.task('partials:build', function() {
+    gulp.src(path.src.partials)
+        .pipe(gulp.dest(path.build.partials));
+});
+
+gulp.task('clean', function (cb) {
+    return gulp.src(path.clean, {read: false}).pipe(plugins.clean());
+});
+
+gulp.task('build', [
+    'clean',
+    'html:build',
+    // 'js:build',
+    'libs:build',
+    'style:build',
+    'fonts:build',
+    'image:build',
+    'partials:build'
+]);
+
